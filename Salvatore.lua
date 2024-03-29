9dc202b29128fc340940db2f5fabf16a09443946
RFR-R1CH4RD <127535312+RFR-R1CH4RD@users.noreply.github.com>
2024-03-29 02:30:35 +0100

    Update Salvatore.lua

--- Salvatore.lua
+++ Salvatore.lua
@@ -112,9 +112,9 @@ local function toggleui()
     ImageButton.MouseButton1Click:Connect(function()
         Toggle = not Toggle
         if Toggle then
-            Window:Minimize()
+            game:GetService('VirtualInputManager'):SendKeyEvent(true, 'LeftAlt', false, uwu)
         else
-            Window:Minimize()
+            game:GetService('VirtualInputManager'):SendKeyEvent(true, 'LeftAlt', false, uwu)
         end
     end)
 
