c1ebde3087af7769acf783aa00b8b63fc633998f
RFR-R1CH4RD <127535312+RFR-R1CH4RD@users.noreply.github.com>
2024-04-03 18:49:03 +0200

    Update Salvatore.lua

--- Salvatore.lua
+++ Salvatore.lua
@@ -95,7 +95,7 @@ local InterfaceManager = {} do
 		end
 
 
-                local MenuKeybind = section:AddKeybind("MenuKeybind", { Title = "Minimize Bind", Default = Settings.MenuKeybind, Description = "You will set minimize bind.", })
+               local MenuKeybind = section:AddKeybind("MenuKeybind", { Title = "Minimize Bind", Default = Settings.MenuKeybind, Description = "You will set minimize bind.", })
 		MenuKeybind:OnChanged(function()
 			Settings.MenuKeybind = MenuKeybind.Value
             InterfaceManager:SaveSettings()
@@ -114,9 +114,8 @@ local InterfaceManager = {} do
 				Settings.Transparency = Value
                 InterfaceManager:SaveSettings()
 			end
-		  end
-    })
+               })
+   end
 end
-
 
 return InterfaceManager
